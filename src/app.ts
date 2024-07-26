43266d37d4ccf10c7841537cb6c78d04484d0e4a
gyeon <https://github.com/phone001>
2024-07-26 17:39:23 +0900

    [feat] 사용자 관련 화면 추가

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
 import express from 'express';
 import path from 'path';
+import router from '../router/main.router';
+import cors from 'cors';
+import cookie from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookie());
+app.use(express.static(path.join(__dirname, "..", "..", "public")))
+app.use(router);
 
-app.set("/", express.static(path.join(__dirname, "../public")))
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:8000"],
+    credentials: true
+}))
+
 app.set("view engine", "ejs");
-app.set("views", path.join("views"));
+app.set("views", path.join(__dirname, "..", "..", "views"));
+
 
 app.get("/", (req, res) => {
-    res.send("hi");
+    // res.send("hi");
+    const { token } = req.cookies;
+    const tokenEmpty = token ? false : true;
+    res.render("index", { tokenEmpty });
 })
 
 app.listen(8000, () => {
