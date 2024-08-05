b4a2d3bebe04bf95748263050c11d08828ea13d7
gyeon <https://github.com/phone001>
2024-08-06 00:38:50 +0900

    [fix]로그인 방법 변경

--- public/js/user/signin.js
+++ public/js/user/signin.js
@@ -5,13 +5,27 @@ window.onload = () => {
         const content = document.querySelector(".content");
         header.style.display = "flex";
         content.hidden = false;
-    }, 800);
+    }, 500);
     const _img = document.querySelector("#logo");
 
 
     _img.onclick = () => {
         location.href = location.protocol + "//" + location.hostname + ":" + location.port
     }
+}
+
+const signin = async () => {
+    const _form = document.querySelector("#form");
+    const { loginId, password, oauthType } = _form;
+    await axios.post("https://testcoffeetree.store/user/signin",
+        {
+            loginId: loginId.value,
+            password: password.value,
+            oauthType: oauthType.value
+        },
+        {
+            withCredentials: true
+        })
 }
 
 const openKakao = () => {
