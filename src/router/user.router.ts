ec28bba96003a0febd0b9918a37eeb32b3cbcb06
gyeon <https://github.com/phone001>
2024-08-06 13:02:51 +0900

    [fix] 쿠키 생성 위치 수정

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -3,9 +3,17 @@ import axios from 'axios';
 const router = Router();
 
 router.post("/signin", async (req, res) => {
-    const { body } = req
-    await axios.post("https://testcoffeetree.store/user/signin", body, { withCredentials: true });
-    res.redirect("/");
+    try {
+        const { body } = req
+        const { data } = await axios.post("https://testcoffeetree.store/user/signin", body, { withCredentials: true });
+        const date = new Date();
+
+        date.setMinutes(date.getMinutes() + 60);
+        res.cookie("token", data.token, { httpOnly: true, expires: date })
+        res.redirect("/");
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 router.get("/signin", async (req, res) => {
