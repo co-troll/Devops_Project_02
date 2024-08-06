6d0f016e1a35c9d07d0c5ce27129ab89f9dece60
gyeon <https://github.com/phone001>
2024-08-06 15:15:24 +0900

    [fix]쿠키 수정

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -13,7 +13,7 @@ router.post("/signin", async (req, res) => {
         const { data } = await axios.post("https://testcoffeetree.store/user/signin", body, { withCredentials: true });
         const date = new Date();
         date.setMinutes(date.getMinutes() + 60)
-        res.cookie("token", data.token, { httpOnly: true, expires: date, sameSite: "none", secure: true });
+        res.cookie("token", data.token, { httpOnly: true, expires: date, sameSite: "none", secure: true, domain: "https://testcoffeetree.store", path: "/" });
         res.redirect("/");
     } catch (error) {
         console.error("")
